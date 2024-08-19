1d5f809abdd16d362cf29cacca67a59108f5daeb
Wagner Vasconcelos <wagner.vasconcelos@infosistemas.com.br>
2024-08-19 15:37:07 -0300

    [*] Configuração Layout Principal

--- uPrincipal.pas
+++ uPrincipal.pas
@@ -4,10 +4,27 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, ExtCtrls, StdCtrls, pngimage, jpeg, Buttons;
 
 type
-  TForm1 = class(TForm)
+  TfrmPrincipal = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Image7: TImage;
+    Panel5: TPanel;
+    Label1: TLabel;
+    btnTreinos: TSpeedButton;
+    btnConfiguracao: TSpeedButton;
+    btnRelatorios: TSpeedButton;
+    btnFinanceiro: TSpeedButton;
+    btnAlunos: TSpeedButton;
+    Edit1: TEdit;
+    Image1: TImage;
+    Shape1: TShape;
+    pnlRender: TPanel;
+    btnHome: TSpeedButton;
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -15,10 +32,16 @@ type
   end;
 
 var
-  Form1: TForm1;
+  frmPrincipal: TfrmPrincipal;
 
 implementation
 
 {$R *.dfm}
+
+procedure TfrmPrincipal.FormShow(Sender: TObject);
+begin
+//  btnAlunos.Glyph.LoadFromFile('C:\Users\wagner.vasconcelos\Downloads\user (2).png');
+// $00301300
+end;
 
 end.
